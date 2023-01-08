890dd73bfeb2db8a320211d92c6069c551cd860c
Marin Teskera <marin.teskera@infinum.com>
2023-01-08 11:32:22 +0100

    create parameter model

--- backend/models/ProcessModel.js
+++ backend/models/ProcessModel.js
@@ -1,154 +1,163 @@
 const db = require("../db/index");
 
-module.exports = class User{
-    constructor(processid,name,start_datetime, end_datetime, description,userId){
-        this.name = name;
-        this.processid = processid;
-        this.start_datetime=start_datetime;
-        this.end_datetime=end_datetime;
-        this.description=description;
-        this.userId=userId;
-    }
+class Process {
+  constructor(
+    processid,
+    name,
+    start_datetime,
+    end_datetime,
+    description,
+    userId
+  ) {
+    this.name = name;
+    this.processid = processid;
+    this.start_datetime = start_datetime;
+    this.end_datetime = end_datetime;
+    this.description = description;
+    this.userId = userId;
+  }
 
-    static async getUserFavProcess(userId){
-        const sql = `
-            select favorite_templates.*, name, start_datetime, end_datetime,description
-            from favorite_templates
-            JOIN process ON process.processid = favorite_templates.processid
-            WHERE favorite_templates.userid = $1
+  static async getUserFavProcess(userId) {
+    const sql = `
+        select favorite_templates.*, name, start_datetime, end_datetime,description
+        from favorite_templates
+        JOIN process ON process.processid = favorite_templates.processid
+        WHERE favorite_templates.userid = $1
         `;
 
-        try{
-            const result =await db.query(sql,[userId]);
-            console.log(result);
-            return result.rows;
-        }catch (e){
-            console.log(e);
-            return null;
-        }
+    try {
+      const result = await db.query(sql, [userId]);
+      console.log(result);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
+      return null;
     }
+  }
 
-    static async getUserProcess(userId){
-        const sql = `
-            SELECT * FROM process where userid = $1;
+  static async getUserProcess(userId) {
+    const sql = `
+        SELECT * FROM process where userid = $1;
         `;
 
-        try{
-            const result =await db.query(sql,[userId]);
-            return result.rows;
-        }catch (e){
-            console.log(e);
-            return null;
-        }
+    try {
+      const result = await db.query(sql, [userId]);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
+      return null;
     }
+  }
 
-    static async deleteFav(processId, userId){
-        const sql = `
-            DELETE FROM favorite_templates
-            WHERE processid = $1 AND userid = $2; 
+  static async deleteFav(processId, userId) {
+    const sql = `
+        DELETE FROM favorite_templates
+        WHERE processid = $1 AND userid = $2; 
         `;
 
-        try{
-            const result = await db.query(sql,[processId,userId]);
-            return !!result.rows;
-
-        }catch (e){
-            console.log(e);
-            return false;
-        }
-
+    try {
+      const result = await db.query(sql, [processId, userId]);
+      return !!result.rows;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
+  }
 
-    static async saveFav(processId,userId){
-        const sql = `
+  static async saveFav(processId, userId) {
+    const sql = `
         INSERT INTO favorite_templates
         (processid, userid)
         VALUES 
         ($1,$2);
         `;
 
-        try{
-            const result = await db.query(sql,[processId,userId]);
-            return !!result.rows;
-
-        }catch (e){
-            console.log(e);
-            return false;
-        }
+    try {
+      const result = await db.query(sql, [processId, userId]);
+      return !!result.rows;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
+  }
 
-    static async getFavProcesses(id){
-        let sql = `
-            SELECT processId FROM favorite_templates WHERE userid = $1
+  static async getFavProcesses(id) {
+    let sql = `
+        SELECT processId FROM favorite_templates WHERE userid = $1
         `;
 
-        try{
-            let result = await db.query(sql,[id]);
-            return result.rows;
-        }catch (e){
-            console.log(e);
-            return false;
-        }
+    try {
+      let result = await db.query(sql, [id]);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
+  }
 
-
-    async insertNewProcess(){
-        let sql = `
+  async insertNewProcess() {
+    let sql = `
         INSERT INTO process 
         (name,start_datetime, end_datetime, description, userId)
         VALUES
         ($1,$2,$3,$4,$5);
         `;
 
-        try{
-            let result = await db.query(sql, [this.name, this.start_datetime, this.end_datetime, this. description, this.userId]);
-            return result.rowCount > 0;
-        }catch (e){
-            console.log(e);
-            return false;
-        }
+    try {
+      let result = await db.query(sql, [
+        this.name,
+        this.start_datetime,
+        this.end_datetime,
+        this.description,
+        this.userId,
+      ]);
+      return result.rowCount > 0;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
+  }
 
-    static async getProcess(id){
-        let sql = `
+  static async getProcess(id) {
+    let sql = `
         SELECT * FROM process_with_phases where processid = $1;
         `;
 
-        try{
-            let result = await db.query(sql, [id]);
-            return result.rows;
-        }catch (e){
-            console.log(e);
-            return false;
-        }
-
+    try {
+      let result = await db.query(sql, [id]);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
+  }
 
-
-    static async getProcessOfUser(id){
-        let sql = `
+  static async getProcessOfUser(id) {
+    let sql = `
         SELECT * FROM process where userid = $1;
         `;
 
-        try{
-            let result = await db.query(sql, [id]);
-            return result.rows;
-        }catch (e){
-            console.log(e);
-            return false;
-        }
+    try {
+      let result = await db.query(sql, [id]);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
-    static async getAllProcess(){
-        let sql = `
-        SELECT * FROM process;
-        `;
+  }
 
-        try{
-            let result = await db.query(sql, []);
-            return result.rows;
-        }catch (e){
-            console.log(e);
-            return false;
-        }
+  static async getAllProcesses() {
+    let sql = `SELECT * FROM process;`;
+
+    try {
+      let result = await db.query(sql, []);
+      console.log("results", result.rows);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
+  }
 }
+
+module.exports = { Process: Process };
--- backend/routes/process.routes.js
+++ backend/routes/process.routes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Process = require("../models/ProcessModel");
+const { Process } = require("../models/ProcessModel");
 
 router.get("/all", (req, res, next) => {
   (async () => {
-
-    let result = await Process.getAllProcess();
+    let result = await Process.getAllProcesses();
 
     if (!result) {
       res.status(501);
@@ -16,94 +15,95 @@ router.get("/all", (req, res, next) => {
   })();
 });
 
-router.get("/user/:userid", (req,res) => {
+router.get("/user/:userid", (req, res) => {
   (async () => {
-
-    let userId=parseInt(req.params.userid);
+    let userId = parseInt(req.params.userid);
     console.log(userId);
-    if(Number.isNaN(userId)){
-      res.status(400)
-      res.send("Please check your request, id should be number")
+    if (Number.isNaN(userId)) {
+      res.status(400);
+      res.send("Please check your request, id should be number");
       return;
     }
 
     let result = await Process.getUserProcess(userId);
 
-    if(!result){
+    if (!result) {
       res.status(501);
       res.send("Server error. Try again later");
       return;
     }
 
     res.send(result);
-
   })();
 });
 
 router.get("/:id", (req, res, next) => {
-    (async () => {
-      let id=parseInt(req.params.id);
-      if(Number.isNaN(id)){
-        res.status(400)
-        res.send("Please check your request, id should be number")
-        return;
-      }
-      let result = await Process.getProcess(id);
-      let count=result.length;
-      if(count===0) {
-        res.status(404)
-        res.send("No process with given id")
-        return;
-      }
-      if (!result) {
-        res.status(501);
-        res.send("Problems with sign in.Please try again later.");
-        return;
-      }
-      res.send(result[0]);
-    })();
+  (async () => {
+    let id = parseInt(req.params.id);
+    if (Number.isNaN(id)) {
+      res.status(400);
+      res.send("Please check your request, id should be number");
+      return;
+    }
+    let result = await Process.getProcess(id);
+    let count = result.length;
+    if (count === 0) {
+      res.status(404);
+      res.send("No process with given id");
+      return;
+    }
+    if (!result) {
+      res.status(501);
+      res.send("Problems with sign in.Please try again later.");
+      return;
+    }
+    res.send(result[0]);
+  })();
 });
 
 router.get("/user/:id", (req, res, next) => {
-    (async () => {
-      let id=parseInt(req.params.id);
-      if(Number.isNaN(id)){
-        res.status(400)
-        res.send("Please check your request, id should be number")
-        return;
-      }
-      let result = await Process.getProcessOfUser(id);
-      let count=result.length;
-      if(count===0) {
-        res.status(404)
-        res.send("No process with given userid")
-        return;
-      }
-      if (!result) {
-        res.status(501);
-        res.send("Problems with sign in.Please try again later.");
-        return;
-      }
-      res.send(result[0]);
-    })();
+  (async () => {
+    let id = parseInt(req.params.id);
+    if (Number.isNaN(id)) {
+      res.status(400);
+      res.send("Please check your request, id should be number");
+      return;
+    }
+    let result = await Process.getProcessOfUser(id);
+    let count = result.length;
+    if (count === 0) {
+      res.status(404);
+      res.send("No process with given userid");
+      return;
+    }
+    if (!result) {
+      res.status(501);
+      res.send("Problems with sign in.Please try again later.");
+      return;
+    }
+    res.send(result[0]);
+  })();
 });
 
 router.post("/create", (req, res, next) => {
-    (async () => {
-  
-      let process = new Process(req.body.name, req.body.start_datetime, 
-                                req.body.end_datetime, req.body.description, 
-                                req.body.userid);
-  
-      let result = await user.insertNewUser();
-  
-      if (!result) {
-        res.status(501);
-        res.send("Problems with sign in.Please try again later.");
-        return;
-      }
-      res.send("ok");
-    })();
-  });
-  
+  (async () => {
+    let process = new Process(
+      req.body.name,
+      req.body.start_datetime,
+      req.body.end_datetime,
+      req.body.description,
+      req.body.userid
+    );
+
+    let result = await user.insertNewUser();
+
+    if (!result) {
+      res.status(501);
+      res.send("Problems with sign in.Please try again later.");
+      return;
+    }
+    res.send("ok");
+  })();
+});
+
 module.exports = router;
