c29d0155a2b2c8e751704018f3e18389ecbfaccc
Marin Teskera <marin.teskera@infinum.com>
2023-01-08 11:41:25 +0100

    create process component model

---
+++ backend/models/ProcessComponentModel.js
@@ -0,0 +1,31 @@
+const db = require("../db/index");
+
+class ProcessComponent {
+  constructor({ componentid, name, phaseid, has_componentid }) {
+    this.componentid = componentid;
+    this.name = name;
+    this.phaseid = phaseid;
+    this.has_componentid = has_componentid;
+  }
+
+  async addProcessComponent() {
+    const sql = `
+			INSERT INTO process_component (name, phaseid, has_componentid)
+			VALUES ($1, $2, $3)
+			RETURNING componentid;
+		`;
+
+    try {
+      const result = await db.query(sql, [
+        this.name,
+        this.phaseid,
+        this.has_componentid,
+      ]);
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  }
+}
+
+module.exports = { ProcessComponent: ProcessComponent };
