bc4c46d7af50778294eaba767381ac021e276871
Ena <ena.sarajlich@gmail.com>
2023-01-19 19:12:46 +0100

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,15 +6,19 @@ export default function Home(){
         <div class="grid-container">
             <div class="grid-item"> 
                 <b>Who are we?</b>  <br></br>
-                We are a group of students at University of Zagreb, Faculty of Electrical Engineering and Computing. This is a project we are doing as a part of our curiculum.
+                We are a group of students at University of Zagreb, Faculty of Electrical Engineering and Computing. 
+                This is a project we are doing as a part of our curiculum.
             </div>
             <div class="grid-item"> 
                 <b>What are we here for?</b>  <br></br>
-                This page offers an creation and overview of product production process tracking. Idea is creating an open platform using blockchain technology where anyone can acces more accurate and confirmable data about each phase of production of any product.
+                This page offers creation and overview of product production process tracking. 
+                The idea is creating an open platform using blockchain technology where anyone 
+                can acces more accurate and confirmable data about each phase of production of any product.
             </div>
             <div class="grid-item"> 
                 <b>Why blockchain?</b>  <br></br>
-                Blockchain stores data at multiple locations, so once information is pushed, it is impossible to change it after. That gives information from our page credibilty.
+                Blockchain stores data at multiple locations, so once information is pushed, 
+                it is impossible to change it after. That gives information from our page credibilty.
             </div>
             <img class="image" src="https://bernardmarr.com/img/What%20Is%20Blockchain%20A%20Super%20Simple%20Guide%20Anyone%20Can%20Understand.png" alt="React Image" />
             <div class="grid-item">
