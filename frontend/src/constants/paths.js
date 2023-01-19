ca37c89eb29cca67e7b693c0ae3e87a528de703f
Dean Trkulja <deantrkulja2001@gmail.com>
2023-01-19 16:55:12 +0100

    Fixing profile backend path

--- frontend/src/constants/paths.js
+++ frontend/src/constants/paths.js
@@ -16,7 +16,7 @@ export const backend_paths = {
     SIGNUP_URL:"http://localhost:3001/registration",
     ONE_PROCESS: "http://localhost:3001/process",
     ALL_PROCESSES:"http://localhost:3001/process/all",
-    ALL_USER_PROCESSES: "http://localhost:3001/process/user",
+    ALL_USER_PROCESSES: "http://localhost:3001/process/byUser",
     ALL_FAV_PROCESSES_CRUD: "http://localhost:3001/favprocess",
     ALL_FAV_USER_PROCESSES_CRUD: "http://localhost:3001/favprocess/user",
     VIEW_PROCESS: "http://localhost:3001/processView",
